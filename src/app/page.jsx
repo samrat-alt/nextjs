6994dc632585a539df9fea3e543ae8a0ce708ed3
samrat-alt <pandeysamrat855@gmail.com>
2024-09-10 18:55:39 +0545

    going good

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+ <section className="py-4">
+  <div className="container mx-auto">
+    <Image src={"https://media.istockphoto.com/id/1346124943/photo/architect-working-on-digital-tablet-at-construction-site.jpg?s=612x612&w=0&k=20&c=5QcuHtYjE7A8ldDe-JJ7uyElhHCzhtZD-2P850mHhok="} width={100} height={100}/>
+  </div>
+ </section>
+   </>
+  );
+}
